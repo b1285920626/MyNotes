84f003e47a362779c9613ce34abd412518a7bb98
b1285920626 <1285920626@qq.com>
2018-08-06 13:45:54 +0800

    2018-08-06

---
+++ Android/MyApplication.java
@@ -0,0 +1,18 @@
+package com.example.fatwhite.long_con_test.util;
+
+import android.app.Application;
+import android.content.Context;
+
+public class MyApplication extends Application {
+    private static Context context;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        context = getApplicationContext();
+    }
+
+    public static Context getContext() {
+        return context;
+    }
+}
